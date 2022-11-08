3b09417e84eb1fee3f9e6e0e5cc1e1c80f045931
Ricardo Massaki <ricardo29puruba@gmail.com>
2022-11-08 18:36:40 -0300

    criando celula e incluindo imagens no projeto tableviewcollectionview

--- TableViewCollectionView/TableViewCollectionView/TableViewCell/CarTableViewCell.swift
+++ TableViewCollectionView/TableViewCollectionView/TableViewCell/CarTableViewCell.swift
@@ -9,25 +9,84 @@ import UIKit
 
 class CarTableViewCell: UITableViewCell {
     
+    //7- fazer a ligacão da label e collectionview
     @IBOutlet weak var titleLabel: UILabel!
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    
+    //5- criou o tableviewcell, já cria o "identifier" e o "nib"
     static let identifier: String = "CarTableViewCell"
-    
+    //5-
     static func nib() -> UINib {
         return UINib(nibName: identifier, bundle: nil)
     }
-
+    
+    //6- colocar o nome no identifier na Xib
+    
+    
+    //18-
+    var listImage:[String] = []
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        //10-
+        configCollectionView()
     }
     
-    func setupCell(nameImage: String) {
-        collectionView.image = UIImage(named: nameImage)
+    //8-
+    private func configCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        //16- qdo criamos uma célula, temos que registra-la
+        collectionView.register(CustomCollectionViewCell.nib(), forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
+        //11-
+        if let layout = self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+            layout.estimatedItemSize = .zero
+        }
     }
+    
+    //17- criar o setup
+    public func setupCell(title: String,listImage:[String]) {
+        self.titleLabel.text = title
+        //19-
+        self.listImage = listImage
+        
+    }
+    
+}
+
+//9-
+extension CarTableViewCell:UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //return 0
+        //20-
+        return self.listImage.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //21-
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: indexPath) as? CustomCollectionViewCell
+        cell?.setupCell(nameImage: self.listImage[indexPath.row])
+        //return UICollectionViewCell()
+        //22-
+        return cell ?? UICollectionViewCell()
+    }
+    
+    //23-
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.contentView.frame.width, height: collectionView.frame.height)
+    }
+    
+}
+        
+    
 
     
     
-}
+    
+
+    
+    
+
--- TableViewCollectionView/TableViewCollectionView/ViewController.swift
+++ TableViewCollectionView/TableViewCollectionView/ViewController.swift
@@ -9,43 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //1- criar a tableview e fazer a ligação
     @IBOutlet weak var tableView: UITableView!
+    
+    //28- criar o array, a lista de imagens
+    var listImage: [String] = ["car1", "car2", "car3", "car4", "car5", "car6"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //3- se criou um método, tem que chamá-lo. é o primeiro ciclo de vida a ser chamado.
         configTableView()
     }
     
+    //2- assinar os protocolos delegate e datasource
     func configTableView() {
         tableView.delegate = self
         tableView.dataSource = self
+        //24-
+        tableView.separatorStyle = .none
+        //25- registrar
         tableView.register(CarTableViewCell.nib(), forCellReuseIdentifier: CarTableViewCell.identifier)
         
+        }
+        
     }
-    
-}
-
-
+//4-
 extension ViewController: UITableViewDelegate {
     
 }
-
-
+//4-
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //26-
         return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //27
         let cell = tableView.dequeueReusableCell(withIdentifier: CarTableViewCell.identifier, for: indexPath) as? CarTableViewCell
-        return cell ?? UITableViewCell()
+        cell?.setupCell(title: "Veículos", listImage: self.listImage)
+        return UITableViewCell()
     }
     
+    //29-
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 182
+        return 180
     }
-    
     
 }
+    
+    
+
 
 
